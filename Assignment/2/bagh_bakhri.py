4d10245b9bc991bc3323044d598d721e7a4605c9
eXcalibur0004 <42509610+eXcalibur0004@users.noreply.github.com>
2021-01-05 21:58:47 +0600

    hudai

---
+++ Assignment/2/bagh_bakhri.py
@@ -0,0 +1,318 @@
+from enum import Enum
+import functools
+import operator
+
+
+class Board(object):
+
+    A0 = 'A0'
+    A2 = 'A2'
+    A4 = 'A4'
+    B1 = 'B1'
+    B3 = 'B3'
+    C0 = 'C0'
+    C2 = 'C2'
+    C4 = 'C4'
+
+    gr_board = {
+        'A0': ['B1', 'C0'],
+        'A1': None,
+        'A2': ['B1', 'C2', 'B3'],
+        'A3': None,
+        'A4': ['B3', 'C4'],
+        'B0': None,
+        'B1': ['A0', 'A2', 'C0', 'C2'],
+        'B2': None,
+        'B3': ['A2', 'C2', 'C4', 'A4'],
+        'B4': None,
+        'C0': ['A0', 'B1'],
+        'C1': None,
+        'C2': ['B1', 'A2', 'B3'],
+        'C3': None,
+        'C4': ['B3', 'A4']
+    }
+
+    def get_gr_board(self, pos):
+        return self.gr_board[pos]
+
+    def is_empty(self, pos):
+        lst_1 = ['G1', 'G2', 'G3', 'T']
+        if self.get(pos) not in lst_1:
+            return True
+        else:
+            return False
+
+    def get(self, pos):
+        if pos == 'A0':
+            return self.A0
+        elif pos == 'A2':
+            return self.A2
+        elif pos == 'A4':
+            return self.A4
+        elif pos == 'B1':
+            return self.B1
+        elif pos == 'B3':
+            return self.B3
+        elif pos == 'C0':
+            return self.C0
+        elif pos == 'C2':
+            return self.C2
+        elif pos == 'C4':
+            return self.C4
+
+    def set(self, pos, name):
+        if pos == 'A0':
+            self.A0 = name
+        elif pos == 'A2':
+            self.A2 = name
+        elif pos == 'A4':
+            self.A4 = name
+        elif pos == 'B1':
+            self.B1 = name
+        elif pos == 'B3':
+            self.B3 = name
+        elif pos == 'C0':
+            self.C0 = name
+        elif pos == 'C2':
+            self.C2 = name
+        elif pos == 'C4':
+            self.C4 = name
+
+    def show(self):
+        str = """
+        {0}        {1}        {2}
+        | \\\ | // | \\\ | // |
+             {3}        {4} 
+        | // | \\\ | // | \\\ |
+        {5}        {6}        {7}
+        """
+        print(str.format(self.A0, self.A2, self.A4,
+                         self.B1, self.B3, self.C0, self.C2, self.C4))
+
+class Animal(object):
+
+    name = ''
+    position = ''
+    status = ''
+    type = ''
+
+    def __init__(self, pos, name, board, type):
+        self.name = name
+        self.set_position(pos, board)
+        self.type = type
+
+    def valid_moves(self, board):
+        return board.get_gr_board(self.position)
+
+    def get_name(self):
+        return self.name
+
+    def get_position(self):
+        return self.position
+
+    def set_position(self, pos, board):
+        self.position = pos
+        board.set(self.position, self.name)
+
+    def move(self, nextpos, board):
+        board.set(self.position, self.position)
+        self.set_position(nextpos, board)
+
+    def get_type(self):
+        return self.type
+
+
+class Goat(Animal):
+
+    def __init__(self, pos, name, board):
+        super().__init__(pos, name, board, 'Goat')
+
+    status = 'Alive'
+    active = False
+
+    def activate(self):
+        active = True
+
+    def set_status(self,s):
+        self.status=s
+
+    def has_backup(self):
+        pass
+
+
+    def get_active(self):
+        return self.active
+
+
+class Tiger(Animal):
+
+    def __init__(self, pos, name, board):
+        super().__init__(pos, name, board, 'Tiger')
+
+    status = 'Free'
+
+class Game(object):
+
+    bd = Board()
+    bd.show()
+    g1 = Goat('A0', 'G1', bd)
+    g2 = Goat('B1', 'G2', bd)
+    g3 = Goat('C0', 'G3', bd)
+    t = Tiger('B3', 'T', bd)
+    bd.show()
+
+    def get_animal(self, name):
+        if name == 'G1':
+            return self.g1
+        elif name == 'G2':
+            return self.g2
+        elif name == 'G3':
+            return self.g3
+        elif name == 'T':
+            return self.t
+        else:
+            return None
+
+    def goat_is_safe(self, g):
+        self_animal = self.get_animal(g)
+
+        _safe_connections={ 'A0':['C0','A2','B1'], 'C0':['A0','B1','C2'],'B1':['A0','A2','C2','C0'],
+                            'A2':['C2','A4','B3','A0'],'C2':['B3','C4','A2','C0'],'B3':['A2','C2','A4','C4'],
+                            'A4':['B3','A2','C4'],'C4':['A4','B3','C2']
+        }
+        safe_coord=_safe_connections[self_animal.get_position()]
+        print(safe_coord)
+        for pos in safe_coord:
+            if pos == self.g1.get_position():
+                return True
+            elif pos == self.g2.get_position():
+                return True
+            elif pos == self.g3.get_position():
+                return True
+        return False
+        # f_depth = self.bd.get_gr_board(self_animal.get_position())
+        # print(f_depth)
+
+        # if self_animal.get_type() == 'Goat':
+        #     if self.g1.get_position() in f_depth:
+        #         print(self.g1.get_position())
+        #         return True
+        #     elif self.g2.get_position() in f_depth:
+        #         print(self.g2.get_position())
+        #         return True
+        #     elif self.g3.get_position() in f_depth:
+        #         print(self.g3.get_position())
+        #         return True
+        #     else:
+        #         print("None")
+        #         return False
+        # else:
+        #     print("Tiger")
+        #     return False
+
+    def kill(self):
+        tiger_pos=self.t.get_position()
+        possible_goat_pos=self.bd.get_gr_board(tiger_pos)
+        print()
+        for pos in possible_goat_pos:
+            if pos == self.g1.get_position():
+                if not self.goat_is_safe(self.get_animal(self.g1)):
+                    self.g1.set_status('Dead')
+                    next_pos = self.g1.get_position()
+                    self.g1.set_position(None,self.bd)
+                    self.t.move(next_pos,self.bd)
+                    return True
+
+            elif pos == self.g2.get_position():
+                if not self.goat_is_safe(self.get_animal(self.g2)):
+                    self.g2.set_status('Dead')
+                    next_pos = self.g2.get_position()
+                    self.g2.set_position(None,self.bd) 
+                    self.t.move(next_pos,self.bd)
+                    return True
+            elif pos == self.g3.get_position():
+                if not self.goat_is_safe(self.get_animal(self.g3)):
+                    self.g3.set_status('Dead')
+                    next_pos = self.g3.get_position()
+                    self.g3.set_position(None,self.bd)
+                    self.t.move(next_pos,self.bd)
+                    return True
+
+    def minmax(self):
+        # if a leaf node is reached, return the score
+        # find the minimum attainable value for the minimizer
+            # first make the move
+            # go deeper in the search tree recursively
+            # then revert the move
+        # find the maximum attainable value for the maximizer
+            # first make the move
+            # go deeper in the search tree recursively
+            # then revert the move
+        pass
+
+    def show_possible_move(self, an):
+        anm = self.get_animal(an)
+        count = 0
+
+        res = anm.valid_moves(self.bd)
+
+        if self.g1.get_position() in res:
+            res.remove(self.g1.get_position())
+        if self.g2.get_position() in res:
+            res.remove(self.g2.get_position())
+        if self.g3.get_position() in res:
+            res.remove(self.g3.get_position())
+        if self.t.get_position() in res:
+            res.remove(self.t.get_position())
+        if res:
+            print("Possible Moves of " + anm.get_name() + ":")
+            for _ in res:
+                print(str(count) + ": " + _)
+                count += 1
+            q = int(input("Please enter a number from above: "))
+            return res[q]
+        else:
+            print("No possible moves for " + anm.get_name() + ".")
+            return None
+
+    def get_board(self):
+        return self.bd
+
+
+def main():
+
+    game = Game()
+    print(game.get_board().get_gr_board('B1'))
+    while not game.kill():
+        
+        print("""
+Enter Animal: 
+1: G1
+2: G2
+3: G3
+4: T
+            """)
+        # print("Enter Number: ")
+        n = int(input("Enter Number: "))
+        if n == 1:
+            x = 'G1'
+        elif n == 2:
+            x = 'G2'
+        elif n == 3:
+            x = 'G3'
+        elif n == 4:
+            x = 'T'
+        # print(game.get_board().get_gr_board('B1'))
+        s = game.show_possible_move(x)
+        if s:
+            #print(game.get_board().get_gr_board('B1'))
+            game.get_animal(x).move(s, game.get_board())
+            game.get_board().show()
+            print(game.goat_is_safe(x))
+        else:
+            # print(game.get_board().get_gr_board('B1'))
+            pass
+
+
+if __name__ == '__main__':
+    main()
--- Assignment/2/gui.py
+++ Assignment/2/gui.py
@@ -1,13 +1,33 @@
 from tkinter import*
 from PIL import ImageTk,Image
+    class Adapter(self):
+        G1_pos
+        G2_pos
+        G3_pos
+        T_pos
+        __init__(self,g1_pos,g2_pos,g3_pos,t_pos):
+            self.G1_pos=g1_pos
+            self.G2_pos=g2_pos
+            self.G3_pos=g3_pos
+            self.T_pos=t_pos
+
+
+#from bagh_bakhri import Animal,Goat,Tiger
 root = Tk()
 root.config(bg="")
 root.title("Baag-Bakhri")
 
 
+
+goat_img_URL='image/goat.png'
+tiger_img_URL='image/tiger.png'
+default_img_URL='image/default.png'
+
+
 def click():
     print("Hellow world!")
 # ver_strip_image = PhotoImage(file='image/rsz_ver_strip.png')
+# LDRU-> Left down right up
 diagonal_arrow_img_LDRU = ImageTk.PhotoImage(Image.open('image/ldru.png'))
 diagonal_arrow_lbl_LDRU_1=Label(image= diagonal_arrow_img_LDRU)
 diagonal_arrow_lbl_LDRU_2=Label(image= diagonal_arrow_img_LDRU)
@@ -19,7 +39,7 @@ diagonal_arrow_lbl_LDRU_2.grid(row=3,column=2)
 diagonal_arrow_lbl_LDRU_3.grid(row=1,column=8)
 diagonal_arrow_lbl_LDRU_4.grid(row=3,column=6)
  
-
+#RULD-> Right up left down
 diagonal_arrow_img_RULD = ImageTk.PhotoImage(Image.open('image/ruld.png'))
 diagonal_arrow_lbl_RULD_1=Label(image= diagonal_arrow_img_RULD)
 diagonal_arrow_lbl_RULD_2=Label(image= diagonal_arrow_img_RULD)
@@ -54,18 +74,44 @@ vertical_arrow_lable_7.grid(row=1,column=9)
 vertical_arrow_lable_8.grid(row=2,column=9)
 vertical_arrow_lable_9.grid(row=3,column=9)
 
-default_image = PhotoImage(file='image/default.png')
-goat_image = PhotoImage(file="image/goat.png")
-tiger_image = PhotoImage(file="image/tiger.png")
+default_image = PhotoImage(file='')
+goat_image = PhotoImage(file=goat_img_URL)
+tiger_image = PhotoImage(file="")
 
-button_A1 = Button(root, border=0, image=goat_image, command=click).grid(row=0,column=0)
-button_C1 = Button(root, border=0, image=goat_image, command=click).grid(row=4,column=0)
-button_B2 = Button(root, border=0, image=goat_image, command=click).grid(row=2,column=3)
-button_A3 = Button(root, border=0, image=default_image, command=click).grid(row=0,column=5)
-button_C3 = Button(root, border=0, image=default_image, command=click).grid(row=4,column=5)
-button_B4 = Button(root, border=0, image=tiger_image, command=click).grid(row=2,column=7)
-button_A5 = Button(root, border=0, image=default_image, command=click).grid(row=0,column=9)
-button_C5 = Button(root, border=0, image=default_image, command=click).grid(row=4,column=9)
+def click_A0(self, animal):
+    pass
+
+def click_A2(self, animal):
+    pass
+
+def click_A4(self, animal):
+    pass
+
+def click_B1(self,animal):
+    pass
+
+def click_B3(self, animal):
+    pass
+
+def click_C0(self, animal):
+    pass
+
+def click_C2(self, animal):
+    pass
+
+def click_C3(self, animal):
+    pass
+
+button_A0 = Button(root, border=0, image=goat_image, command=click_A0).grid(row=0,column=0)
+button_A2 = Button(root, border=0, image=goat_image, command=click_A2).grid(row=4,column=0)
+button_A4 = Button(root, border=0, image=goat_image, command=click_A4).grid(row=2,column=3)
+button_B1 = Button(root, border=0, image=default_image, command=click_B1).grid(row=0,column=5)
+button_B3 = Button(root, border=0, image=default_image, command=click_B3).grid(row=4,column=5)
+button_C0 = Button(root, border=0, image=tiger_image, command=click_C0).grid(row=2,column=7)
+button_C2 = Button(root, border=0, image=default_image, command=click_C2).grid(row=0,column=9)
+button_C4 = Button(root, border=0, image=default_image, command=click_C3).grid(row=4,column=9)
+
+
 
 def command():
     print("hellow world!")
